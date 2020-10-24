780603e54b0b35f756842bdc827f7e206eb0a362
Saecki <tobiasschmitz2001@gmail.com>
2020-10-24 18:26:14 +0200

    changes

--- wippe_arduino.ino
+++ wippe_arduino.ino
@@ -3,30 +3,15 @@
 #define servoMin 20
 #define servoMax 160
 
-#define PF 0.7
+#define PF 0
 #define IF 0
 #define DF 180
 
 Servo servoX;
 Servo servoY;
 
-long currentTime = 0;
-long lastUpdateTime = 0;
-float calX = 0;
-float calY = 0;
-float ballX = 0.5;
-float ballY = 0.5;
 float posX = 0.5;
 float posY = 0.5;
-float lastDiffX = 0;
-float lastDiffY = 0;
-
-float px = 0;
-float py = 0;
-float dx = 0;
-float dy = 0;
-float ix = 0;
-float iy = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -49,48 +34,15 @@ void loop() {
         String strX = Serial.readStringUntil(',');
         String strY = Serial.readStringUntil('\n');
 
-        while (Serial.available()) {
-            Serial.read();
-        }
+        if (strX.length() == 0) posX = 0.5;
+        else posX = strX.toFloat();
 
-        if (strX.length() == 0) ballX = 0.5;
-        else ballX = strX.toFloat();
-
-        if (strY.length() == 0) ballY = 0.5;
-        else ballY = -strY.toFloat() + 1;
-
-        currentTime = millis();
-    
-        float diffX = (0.5 - ballX);
-        float diffY = (0.5 - ballY);
-        float timeDiff = currentTime - lastUpdateTime;
-        
-        px = diffX * PF;
-        py = diffY * PF;
-
-        ix += diffX * IF / timeDiff;
-        iy += diffY * IF / timeDiff;
-
-        dx = sq(diffX - lastDiffX) * DF / timeDiff;
-        dy = sq(diffY - lastDiffY) * DF / timeDiff;
-         
-        posX = 0.5 + px + ix + dx;
-        posY = 0.5 + py + iy + dy;
-
-        lastDiffX = diffX;
-        lastDiffY = diffY;
-
-        lastUpdateTime = millis();
+        if (strY.length() == 0) posY = 0.5;
+        else posY = -strY.toFloat() + 1;
     }
 
     servoX.write(servoValue(posX));
     servoY.write(servoValue(posY));
-
-    Serial.print("bx:");
-    Serial.print(ballX);
-    Serial.print("by:");
-    Serial.print(ballY);
-    Serial.println();
 }
 
 int servoValue(float val) {
